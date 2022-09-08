63fc2f5992032b18c84c2f9e7726b8c87ef69087
Matias Aguirre <aguirremati09@gmail.com>
2022-09-08 11:32:36 -0300

    Changed class based component to a functional component with hooks.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      next: null,
-      operation: null,
-      total: null,
-    };
-    this.clickHandler = this.clickHandler.bind(this);
-  }
+const Calculator = () => {
+  const [state, SetState] = useState({});
+  const clickHandler = (e) => {
+    SetState(calculate(state, e.target.textContent));
+  };
+  const { next, operation, total } = state;
+  return (
+    <div className="calculator">
+      <div className="display">{ next || operation || total || 0 }</div>
+      <button type="button" onClick={clickHandler} className="gray">AC</button>
+      <button type="button" onClick={clickHandler} className="gray">+/-</button>
+      <button type="button" onClick={clickHandler} className="gray">%</button>
+      <button type="button" onClick={clickHandler} className="orange">÷</button>
+      <button type="button" onClick={clickHandler} className="gray">7</button>
+      <button type="button" onClick={clickHandler} className="gray">8</button>
+      <button type="button" onClick={clickHandler} className="gray">9</button>
+      <button type="button" onClick={clickHandler} className="orange">x</button>
+      <button type="button" onClick={clickHandler} className="gray">4</button>
+      <button type="button" onClick={clickHandler} className="gray">5</button>
+      <button type="button" onClick={clickHandler} className="gray">6</button>
+      <button type="button" onClick={clickHandler} className="orange">-</button>
+      <button type="button" onClick={clickHandler} className="gray">1</button>
+      <button type="button" onClick={clickHandler} className="gray">2</button>
+      <button type="button" onClick={clickHandler} className="gray">3</button>
+      <button type="button" onClick={clickHandler} className="orange">+</button>
+      <button type="button" onClick={clickHandler} className="gray zero">0</button>
+      <button type="button" onClick={clickHandler} className="gray">.</button>
+      <button type="button" onClick={clickHandler} className="orange">=</button>
+    </div>
+  );
+};
 
-  clickHandler(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
-
-  render() {
-    const { next, operation, total } = this.state;
-    return (
-      <div className="calculator">
-        <div className="display">{ next || operation || total || 0 }</div>
-        <button type="button" onClick={this.clickHandler} className="gray">AC</button>
-        <button type="button" onClick={this.clickHandler} className="gray">+/-</button>
-        <button type="button" onClick={this.clickHandler} className="gray">%</button>
-        <button type="button" onClick={this.clickHandler} className="orange">÷</button>
-        <button type="button" onClick={this.clickHandler} className="gray">7</button>
-        <button type="button" onClick={this.clickHandler} className="gray">8</button>
-        <button type="button" onClick={this.clickHandler} className="gray">9</button>
-        <button type="button" onClick={this.clickHandler} className="orange">x</button>
-        <button type="button" onClick={this.clickHandler} className="gray">4</button>
-        <button type="button" onClick={this.clickHandler} className="gray">5</button>
-        <button type="button" onClick={this.clickHandler} className="gray">6</button>
-        <button type="button" onClick={this.clickHandler} className="orange">-</button>
-        <button type="button" onClick={this.clickHandler} className="gray">1</button>
-        <button type="button" onClick={this.clickHandler} className="gray">2</button>
-        <button type="button" onClick={this.clickHandler} className="gray">3</button>
-        <button type="button" onClick={this.clickHandler} className="orange">+</button>
-        <button type="button" onClick={this.clickHandler} className="gray zero">0</button>
-        <button type="button" onClick={this.clickHandler} className="gray">.</button>
-        <button type="button" onClick={this.clickHandler} className="orange">=</button>
-      </div>
-    );
-  }
-}
+export default Calculator;
